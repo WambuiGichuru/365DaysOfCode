86740a5742885ee26d12e0295bc8a70cf45efea7
WambuiGichuru <gichuru.w.faith>
2025-01-15 21:20:14 +0300

    finalizing the black jack game

--- week2/blackjack_game.py
+++ week2/blackjack_game.py
@@ -1,4 +1,4 @@
-   # black jack game
+# black jack game
 import random
 
 def deal_card():
@@ -7,15 +7,64 @@ def deal_card():
     card = random.choice(cards)
     return card
 
-user_cards = []
-comp_cards = []
-
-# to fill the lists created above
-for _ in range(2):
-    new_card = deal_card() # or user_card.append(deal_card())
-    comp_cards.append(deal_card()) # or comp = deal_card()
-
 def calculate_score(cards):
     if sum(cards) == 21 and len(cards) ==2:
         return 0
+    if 11 in cards and sum(cards)> 21:
+        cards.remove(11)
+        cards.append(1)
     return sum(cards)
+
+def compare(user_score, comp_score):
+    if user_score == comp_score:
+        return "Draw"
+    elif comp_score == 0:
+        return "Lose, opponent has blackjack"
+    elif user_score == 0:
+        return "Win with a Blackjack"
+    elif user_score > 21:
+        return "You went over, you lose"
+    elif comp_score > 21:
+        return "Opponent went over, you win"
+    elif  user_score > comp_score:
+        return "You win"
+    else:
+        return "You lose"
+
+
+def play_game():
+    user_cards = []
+    comp_cards = []
+    is_game_over = False
+
+    # to fill the lists created above
+    for _ in range(2):
+        user_cards.append(deal_card()) # or user_card.append(deal_card())
+        comp_cards.append(deal_card()) # or comp = deal_card()
+
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        comp_score = calculate_score(comp_cards)
+
+        print(f"Your cards: {user_cards}, current score: {user_score}")
+        print (f"Computer's first card: {comp_cards[0]}")
+
+        if user_score ==0  or comp_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_deal = input("Type 'y' to get another card, type 'n' to pass: " )
+            if user_deal == 'y':
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    while comp_score != 0 and comp_score < 17:
+        comp_cards.append(deal_card())
+        comp_score= calculate_score(comp_cards)
+
+    print(f"Your final hand: {user_cards}, final score: {user_score} ")
+    print (f" Computer's final hand: {comp_cards}, final score: {comp_score}" )
+    print(compare(user_score, comp_score))
+
+while input("Do you want to play a game of BlackJack? y/n :") == "y":
+    play_game()

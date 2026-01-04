fe91eee6c1aa5685aed69126e7245a211a319416
WambuiGichuru <gichuru.w.faith>
2026-01-04 22:32:09 +0300

    Reviewing some old code and continuous polish on some basics

--- week1/banker_roulette.py
+++ week1/banker_roulette.py
@@ -1,3 +1,4 @@
+"""
 # banker roulette
 import random
 names = input("Give me all the names, seperated by a comma.")
@@ -10,4 +11,17 @@ print(f"Person paying is {choice}")
 
 ch = random.choice(nam)
 print(ch)
+"""
 
+# reviewing basics at the start of another year 2026
+import random 
+na = input(" Input all the names separating using a comma.")
+n = len(na)
+
+no = len(na)
+
+choose = random.randint(0, no -1)
+print(f"{choose}, is paying this time.")
+
+choice = random.choice("choose")
+print(choice)
--- week1/bmi_calculator.py
+++ week1/bmi_calculator.py
@@ -1,3 +1,4 @@
+"""
 # BMI Calculator
 print("Welcome to the BMI calculator")
 height = float(input("Please enter your height in m: "))
@@ -15,4 +16,22 @@ elif BMI < 35:
     print(f"Your BMI is: {BMI} , You are obese")
 else:
     print(f"Your BMI is: {BMI} , Your are clinically obese")
-        
+
+"""
+    
+# recoding this a year later to review my basics and ensure they are stil intact
+print('This is a BMI Calculator')
+heig = float(input('Enter your height in metres:'))
+weig = int(input('Enter weight in kgs: '))
+B = round((weig/heig**2),1)
+
+if B < 18.5:
+    print(f"BMI is: {B}, you are underweight")
+if B < 25:
+    print(f"BMI is: {B}, you are normal weight")
+if B < 30:
+    print(f"BMI is: {B}, you are overweight")
+if B < 35:
+    print(f"BMI is: {B}, you are obese")
+else:
+    print(f"BMI is: {B}, you are clinically obese")

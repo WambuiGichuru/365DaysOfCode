55848253706ef13227b7d574d2f3d1879228d84b
WambuiGichuru <gichuru.w.faith>
2026-01-08 22:03:33 +0300

    more reviews

--- week1/leapyear_checker.py
+++ week1/leapyear_checker.py
@@ -1,3 +1,4 @@
+"""
 print("Welcome to a leap year checker")
 year = int(input("What year do you want to check? "))
 
@@ -11,4 +12,19 @@ if year % 4 == 0:
         print("It is a leap year")
 else:
     print(" Sorry, not a leap year.")
-    
+
+"""
+
+print ("Welcome to a leap year checker")
+year = int(input("What year do you want to check? "))
+
+if year % 4 == 0: 
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("It's a leap year")
+        else:
+            print("Not a leap year")
+    else:
+        print("It is a leap year")
+else:
+    print('Not a leap year')

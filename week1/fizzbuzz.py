ad3bb0fec21dc5da4917c333943e278401920015
WambuiGichuru <gichuru.w.faith>
2026-01-08 21:39:22 +0300

    Another review

--- week1/fizzbuzz.py
+++ week1/fizzbuzz.py
@@ -1,3 +1,4 @@
+"""
 # fizzbuzz exercise
 t = 0
 for num in range(1, 100):
@@ -10,3 +11,17 @@ for num in range(1, 100):
     else:
         print(num)
         
+"""
+
+# trying this again
+l = 0
+for num in range(1,100):
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 5 == 0:
+        print('Buzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    else:
+        print(num)
+

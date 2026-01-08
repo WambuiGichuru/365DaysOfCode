1d75f004d5adee6c776c5acd982265d4269e8327
WambuiGichuru <gichuru.w.faith>
2026-01-08 21:32:35 +0300

    light day of coding, reviewing basics

--- week1/day1.py
+++ week1/day1.py
@@ -1,3 +1,4 @@
+"""
 # Starting out with data types and printing
 name = "Faith"
 print(name)
@@ -10,4 +11,17 @@ print("Welcome to a SuperPower name generator")
 p = input("What's your favourite colour?")
 e = input("What's the last thing you ate?")
 print("Your name is:"+p+e)
+"""
+# trying this again to see if I still got it
+name= 'WAMBUI'
+print(name)
+
+na = input('What is my name')
+le = len(na)
+print(le)
+
+print('Let us give you a superpower name')
+pe = input("What is today's day: ")
+en = input('What are you currently doing? ')
+print(f"Your superpower is: {pe} , {en}")
 

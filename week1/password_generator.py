04270ae1f17b9a871c82b853a35ef00600374e87
WambuiGichuru <gichuru.w.faith>
2026-01-10 22:59:30 +0300

    small changes abeg

--- week1/password_generator.py
+++ week1/password_generator.py
@@ -1,3 +1,4 @@
+"""
 # password generator
 import random
 
@@ -24,3 +25,37 @@ for p in range(1, s +1):
 for p in range(1, n +1):
     password += random.choice(numbers)
 print(password)
+
+"""
+
+# trying this again you know
+# password generator 2.0
+
+import random
+
+lets = letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m'
+        'n','o','p','q','r','s','t','u','v','w','x','y','z'
+    'A','B','C','D','E','F','G','H','I','J','K','L','M',
+    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+nums = ['0','1','2','3','4','5','6','7','8','9']
+symbols = ['!','@','#','$','%','&','*','+','(',')']
+
+print ( 'Welcome to password generator 2.0')
+l = str(input('How many letters would you like in your password?'))
+n = str(input("HOw many numbers would you like in your password?"))
+s = str(input("How many symbols would you like in your password?"))
+
+password = ''
+
+for p in range (1, l +1):
+    password +=random.choice(lets)
+for p in range (1, n +1):
+    password += random.choice(nums)
+for p in range (1, s +1):
+    password += random.choice(symbols)
+
+print(password)
+pass_list = list(password)
+#shuffle it
+shuffled =random.shuffle(pass_list)
+print(shuffled)
